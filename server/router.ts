2af1f2707736dde130b24764c81d9b616213599e
t-yng <t.yng.greenmonkey@gmail.com>
2020-04-12 00:30:53 +0900

    TODOの更新APIを実装

--- server/router.ts
+++ server/router.ts
@@ -24,6 +24,43 @@ router
 
         ctx.response.status = 204;
     })
+    .put('/api/todos/:id', async (ctx) => {
+        const id = ctx.params.id;
+        let body;
+
+        try {
+            // Warning: リクエストBodyは英字のみ有効
+            // 日本語をBodyに含めると文字化けが発生して内部のJOSN.parse()でエラーが発生する
+            body = await ctx.request.body();
+        } catch (error) {
+            console.error(error);
+            ctx.response.status = 500;
+            ctx.response.body = 'Internal Server Error';
+            return;
+        }
+
+        // Content-Type: appliacation/json 以外は受け付けない
+        if (body.type !== BodyType.JSON) {
+            ctx.response.status = 400;
+            ctx.response.body = 'Content-Type is not application/json';
+            return;
+        }
+
+        const { content } = body.value;
+        try {
+            await client.execute('UPDATE todos SET content = ? WHERE id = ?', [
+                content,
+                id,
+            ]);
+        } catch (error) {
+            console.error(error);
+            ctx.response.status = 500;
+            ctx.response.body = 'Internal Server Error';
+            return;
+        }
+
+        ctx.response.status = 204;
+    })
     .post('/api/todos', async (ctx) => {
         let body;
         try {
